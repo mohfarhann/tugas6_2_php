647f972414d2fa7fc0804db61dbe259993ef00fc
mohfarhann <90471536+mohfarhann@users.noreply.github.com>
2022-05-19 14:11:19 +0700

    Add files via upload

---
+++ tugas6_2.php
@@ -0,0 +1,16 @@
+<?php
+
+  $x = 15000;
+  $y = 2000;
+
+  $a = ($x * 5) * 6;
+  $b = $y * 2;
+  $c = $a + $b;
+
+  echo "==============================";
+  echo "</br>Harga Jambu = $x";
+  echo "</br>Harga Dus = $y";
+  echo "</br>Total Penjualan (Dus dan Jambu) = $c";
+  echo "</br>==============================";
+
+?>
